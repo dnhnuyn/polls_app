1a22f576069d7b6e6eff7b01bfaae4fd4ec3a6ba
dnhnuyn <danh3646567@gmail.com>
2026-09-10 10:12:03 +0700

    Initial commit: Django polls project with PostgreSQL

---
+++ polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
+
+from . import views
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
+
