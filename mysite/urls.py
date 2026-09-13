29aa74bce9ef3393c358e3b73acc4381ed39dca4
dnhnuyn <danh3646567@gmail.com>
2026-09-13 13:59:01 +0700

    Create home.html, update views.py and mysite/urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from polls import views as polls_views
 
 urlpatterns = [
+    path("", polls_views.home, name="home"),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls"))
